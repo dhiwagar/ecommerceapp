5c8a53532de0a296afd1e390e1b679e0f804e481
KEPREVOS-DEV-1\kepre <dhiwagar@keprevos.in>
2024-06-11 19:02:22 +0530

    newchages

--- index.js
+++ index.js
@@ -15,8 +15,10 @@ const authRouter = require("./routes/authRoute");
 const categoryRouter = require("./routes/prodcategoryRoute");
 const productRouter =require("./routes/productRoute")
 const brandRouter = require("./routes/brandRoute");
-
+const colorRouter = require("./routes/colorRoute");
 const blogcategoryRouter = require("./routes/blogCatRoute");
+const couponRouter = require("./routes/couponRoute");
+const enqRouter = require("./routes/enqRoute");
 const blogRoute =require("./routes/blogRoute")
 app.use(cookieParser());
 // Middleware to parse JSON requests
@@ -25,9 +27,11 @@ app.use("/api/user", authRouter);
 app.use("/api/blog", blogRoute);
 app.use("/api/blogcategory", blogcategoryRouter);
 app.use("/api/brand", brandRouter);
+app.use("/api/enquiry", enqRouter);
+app.use("/api/coupon", couponRouter);
 app.use("/api/category", categoryRouter);
 app.use("/api/product", productRouter);
-
+app.use("/api/color", colorRouter);
 app.use(notFound);
 app.use(errorHandler);
 app.listen(port, () => {
